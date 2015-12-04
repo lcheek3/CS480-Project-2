a8d62e47c8bf2b472c6466e716ad77b7d433293c
Lucas Cheek <lcheek3@gmail.com>
2015-12-04 08:09:54 -0900

    Cleaned up project 1
    I took some time cleaning up the junk in the project 1 code to make
    things easier.
    The main changes:
    *No more global variables
    *Global constants are now read into a std::map from settings.conf, which
    is passed between functions. No more compiling every time we want to
    change a setting. Could also integrate well with slider bars in a GUI
    (wink wink.)
    *All helper functions are now in trackerFunctions.h

---
+++ Refactored Project 1/main.cpp
@@ -0,0 +1,172 @@
+
+//CS480
+//Project 1
+//Motion Tracking with OpenCV
+//Lucas, Laurin, and Tom
+//10/21/15
+
+//TUTORIALS
+/*
+http://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
+
+http://docs.opencv.org/doc/tutorials/imgproc/shapedescriptors/bounding_rects_circles/bounding_rects_circles.html
+
+https://dl.dropboxusercontent.com/u/28096936/tuts/motionTrackingTut/finalCode/motionTracking.cpp
+*/
+
+
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "trackerFunctions.h"
+
+
+int main() {
+	//object xy
+	cv::Point targ;
+
+	//variables for target path smoothing and rate governor
+	cv::Point current;
+	cv::Point destination;
+	std::vector<cv::Point> samplePoints;
+
+	//variable for trail data
+	std::vector<cv::Point> trailPoints;
+
+	std::vector<cv::Point> targetPoints;
+
+	std::map<std::string, int> configuration = getConfiguration();
+	if (!configurationAutoReplace(configuration)) {
+		configuration = getConfiguration();
+	}
+	for (auto it = configuration.cbegin(); it != configuration.end(); ++it) {
+		std::cout << it->first << " : " << it->second << std::endl;
+	}
+	//Lucas' Target code
+
+	//some boolean variables for added functionality
+	bool objectDetected = false;
+	//these two can be toggled by pressing 'd' or 't'
+	bool debugMode = false;
+	bool trackingEnabled = false;
+	//pause and resume code
+	bool pause = false;
+	//set up the matrices that we will need
+	//the two frames we will be comparing
+	cv::Mat frame1, frame2;
+	//their grayscale images (needed for absdiff() function)
+	cv::Mat grayImage1, grayImage2;
+	//resulting difference image
+	cv::Mat differenceImage;
+	//thresholded difference image (for use in findContours() function)
+	cv::Mat thresholdImage;
+	//video capture object.
+	//VideoCapture capture;
+	cv::VideoCapture stream1(0);
+	if (!stream1.isOpened()) {
+		std::cout << "Cannot open camera." << std::endl;
+	}
+
+	while (1) {
+
+		//read first frame
+		stream1.read(frame1);
+		//convert frame1 to gray scale for frame differencing
+		cvtColor(frame1, grayImage1, cv::COLOR_BGR2GRAY);
+		//copy second frame
+		stream1.read(frame2);
+		//convert frame2 to gray scale for frame differencing
+		cvtColor(frame2, grayImage2, cv::COLOR_BGR2GRAY);
+		//perform frame differencing with the sequential images. This will output an "intensity image"
+		//do not confuse this with a threshold image, we will need to perform thresholding afterwards.
+		absdiff(grayImage1, grayImage2, differenceImage);
+		//threshold intensity image at a given sensitivity value
+		threshold(differenceImage, thresholdImage, configuration["SENSITIVITY_VALUE"], 255, cv::THRESH_BINARY);
+		if (debugMode == true) {
+			//show the difference image and threshold image
+			imshow("Difference Image", differenceImage);
+			imshow("Threshold Image", thresholdImage);
+		}
+		else {
+			//if not in debug mode, destroy the windows
+			cv::destroyWindow("Difference Image");
+			cv::destroyWindow("Threshold Image");
+		}
+		//blur() to smooth the image, remove noise
+		blur(thresholdImage, thresholdImage, cv::Size(configuration["BLUR_SIZE"], configuration["BLUR_SIZE"]));
+		//threshold again to obtain binary image from blur output
+		threshold(thresholdImage, thresholdImage, configuration["SENSITIVITY_VALUE"], 255, cv::THRESH_BINARY);
+		//verifies that image is 8 bit for findcontours()
+		thresholdImage.convertTo(thresholdImage, CV_8U);
+		if (debugMode == true) {
+			//show the threshold image after it's been "blurred"
+			imshow("Final Threshold Image", thresholdImage);
+		}
+		else {
+			cv::destroyWindow("Final Threshold Image");
+		}
+
+		//if tracking enabled, search for contours in our thresholded image
+		if (trackingEnabled) {
+			//Collects a number of sample averages specified by SMOOTHING_SAMPLE and sticks them in the samplePoints global vector
+			collectSamples(thresholdImage, frame1, samplePoints, targ, configuration);
+		}
+		//when samplePoints vector reaches the size specified by SMOOTHING_SAMPLE, updates the current point and clears samplePoints
+		if (samplePoints.size() == configuration["SMOOTHING_SAMPLE"]) {
+			destination = pathSmoothing(frame1, samplePoints, configuration);
+			samplePoints.clear();
+		}
+
+		if (trackingEnabled) {
+			drawTrails(frame1, current, targetPoints, configuration);
+		}
+
+
+		//limits the speed of movement of the target crosshair. We can tweak SPEED_OF_MOVEMENT to accurately reflect the actual position of the laser in project 2
+		//so that we can draw an obscuring circle over the laser
+		current = speedGovernor(current, destination, configuration["SPEED_OF_MOVEMENT"]);
+		cv::Point predictLine = speedGovernor(current, destination, 100);
+
+		//draw the target
+		drawTarget(current, frame1);
+		line(frame1, current, predictLine, cv::Scalar(0, 255, 0), 2);
+
+		imshow("Frame1", frame1);
+
+		//hotkey code from tutorial at (https://dl.dropboxusercontent.com/u/28096936/tuts/motionTrackingTut/finalCode/motionTracking.cpp)
+		switch (cv::waitKey(10)) {
+		case 27: //'esc' key has been pressed, exit program.
+			return 0;
+		case 116: //'t' has been pressed. this will toggle tracking
+			trackingEnabled = !trackingEnabled;
+			if (trackingEnabled == false) std::cout << "Tracking disabled." << std::endl;
+			else std::cout << "Tracking enabled." << std::endl;
+			break;
+		case 100: //'d' has been pressed. this will debug mode
+			debugMode = !debugMode;
+			if (debugMode == false) std::cout << "Debug mode disabled." << std::endl;
+			else std::cout << "Debug mode enabled." << std::endl;
+			break;
+		case 112: //'p' has been pressed. this will pause/resume the code.
+			pause = !pause;
+			if (pause == true) {
+				std::cout << "Code paused, press 'p' again to resume" << std::endl;
+				while (pause == true) {
+					//stay in this loop until 
+					switch (cv::waitKey()) {
+					case 112:
+						//change pause back to false
+						pause = false;
+						std::cout << "Code resumed." << std::endl;
+						break;
+					}
+				}
+			}
+
+		}
+	}
+	return 0;
+}
