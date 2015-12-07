a69bacd4fb1f0ba6459928a33108b799bf807a35
Lucas Cheek <lcheek3@gmail.com>
2015-12-06 16:00:43 -0900

    Quick fix to the getCoordinates function
    The way I had originally written the vertical offset was the exact
    opposite of correct, which as we all know is basically correct. Just
    flipped the fraction in the arctangent so that the offset reduces as
    distance increases.

--- Refactored Project 1/trackerFunctions.h
+++ Refactored Project 1/trackerFunctions.h
@@ -18,7 +18,7 @@
 std::vector<int> getCoordinates(cv::Point destination, std::map<std::string, int> configuration) {
 	double parallax_unit = (tan(0.43)*configuration["PARALLAX"]) / (configuration["CAM_RES_X"] / 2);
 	int dist_cam_turr = 65; //in mm
-	double vertical_offset = atan2(configuration["PARALLAX"], dist_cam_turr)*parallax_unit;
+	double vertical_offset = atan2(dist_cam_turr,configuration["PARALLAX"])*parallax_unit;
 	std::cout << vertical_offset << std::endl;
 	int x_command = -atan2((destination.x - configuration["CAM_RES_X"] / 2)*parallax_unit , configuration["PARALLAX"])*180/CV_PI+90;
 	int y_command = atan2((destination.y - configuration["CAM_RES_Y"] / 2)*parallax_unit , configuration["PARALLAX"])*180/CV_PI+90+vertical_offset;
@@ -71,6 +71,10 @@ bool configurationAutoReplace(std::map<std::string, int> configuration) {
 	}
 	if (!configuration["COM"]) {
 		openFile << "COM = 5\n";
+		unchanged = false;
+	}
+	if (!configuration["CAMERA"]) {
+		openFile << "CAMERA = 0\n";
 		unchanged = false;
 	}
 	return unchanged;
